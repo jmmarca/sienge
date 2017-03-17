ace1ba789d331dd4a061a5e9bff6b6afb9291964
Jean Michel Marca <michel@smartbr.com>
2017-03-17 18:26:09 -0300

    Correção de Cadastros

---
+++ sienge-web/src/main/java/com/softplan/model/util/EntidadeConverter.java
@@ -0,0 +1,32 @@
+package com.softplan.model.util;
+
+import com.softplan.model.generic.Entidade;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(forClass = Entidade.class)
+public class EntidadeConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
+        if (value != null && !value.isEmpty()) {
+            return (Entidade) uiComponent.getAttributes().get(value);
+        }
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object value) {
+        if (value instanceof Entidade) {
+            Entidade entity = (Entidade) value;
+            if (entity != null && entity instanceof Entidade && entity.getId() != null) {
+                uiComponent.getAttributes().put(entity.getId().toString(), entity);
+                return entity.getId().toString();
+            }
+        }
+        return "";
+    }
+
+}
